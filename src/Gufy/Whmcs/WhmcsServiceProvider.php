42b1bc7bf27b30447721a1d0938a50f542ea94e1
Atef Ben Ali <atef.bettaib@gmail.com>
2018-10-18 17:07:21 +0100

    style: format

--- src/Gufy/Whmcs/WhmcsServiceProvider.php
+++ src/Gufy/Whmcs/WhmcsServiceProvider.php
@@ -2,57 +2,53 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class WhmcsServiceProvider extends ServiceProvider {
+class WhmcsServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('gufy/whmcs');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('gufy/whmcs');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['Whmcs'] = $this->app->share(function () {
 
-		$this->app['Whmcs'] = $this->app->share(function() {
-			
-			return new Whmcs();
+            return new Whmcs();
 
-		});
+        });
 
-		$this->app->booting(function() {
-		  
-		  $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $this->app->booting(function () {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
 
-		  $loader->alias('Whmcs', 'Gufy\Whmcs\Facades\Whmcs');
+            $loader->alias('Whmcs', 'Gufy\Whmcs\Facades\Whmcs');
+        });
+    }
 
-		});
-
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('Whmcs');
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('Whmcs');
+    }
 }
